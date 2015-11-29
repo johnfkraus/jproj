c57f3a6a14e6311661eeca9b60b19d5c94a1b330
johnfkraus <johnkraus3@gmail.com>
2015-11-29 06:06:50 -0500

    added static root for pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # required by pythonanywhere
 
 SITE_ID = 1
 
